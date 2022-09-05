9173a9f542c4790b0caa41a5c5ffb40b30a1edce
Sandeep Bisht <>
2022-09-05 15:54:38 +0530

    full paper download

--- controller/DownloadFullPaperList/index.js
+++ controller/DownloadFullPaperList/index.js
@@ -1,22 +1,39 @@
 const excel = require("exceljs");
+const merge = require('deepmerge')
+let registredUserInfo = require('../../models/registredUserInfo')
 let fullPaper = require('../../models/fullPaper')
 
 exports.downloadFullPaperList = async (req, res) => {
+    let response = await registredUserInfo.find({}, {designation:1, conferenceMode:1, registrationCategory:1, participationType:1, email:1})    
+    let arr = []
+    const usersList = await fullPaper.find()
+    for(let el of response) {
+        for(item of usersList) {
+            if(el.email === item.userEmail) {
+                const merged = merge(el, item)
+                arr.push(merged._doc)
+            }
+        }
+    }
     try {
         let workbook = new excel.Workbook();
-        let worksheet = workbook.addWorksheet("fullPaper");
+        let worksheet = workbook.addWorksheet("arr");
         worksheet.columns = [
             {header: "Registration Number", key:"registrationNumber"},
             { header: "Name", key: "userName", },
             { header: "Email", key: "userEmail", },
             { header: "Document", key: "fullPaperName",},
-            {header:"Theme", key:"themeType",}
+            {header:"Theme", key:"themeType",},
+            { header: "Designation", key:"designation"},
+            { header: "Conference Mode", key: "conferenceMode", },
+            { header: "Registration Category", key: "registrationCategory" },
+            { header: "Participation Type", key: "participationType"}
         ];
 
         // Add Array Rows
-        const usersList = await fullPaper.find()
+       
 
-        worksheet.addRows(usersList);
+        worksheet.addRows(arr);
         res.setHeader(
             "Content-Type",
             "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
