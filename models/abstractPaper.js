f8504e2b4eaa87ccadc45898affaf84c968a43c8
Sandeep Bisht <>
2023-07-22 18:36:46 +0530

    handle abstarct page abstarct paper ID

--- models/abstractPaper.js
+++ models/abstractPaper.js
@@ -1,16 +1,22 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 const abstractPaperSchema = new Schema({
-    userName: {type : String, require:true },
-    userEmail: {type : String, require:true},
+    authorSaluation: {type : String, require:true },
+    authorFirstName: {type : String, require:true },
+    authorMiddleName: {type : String, require:true },
+    authorLastName: {type : String, require:true },
+    authorEmail: {type : String, require:true },
+    authorAffiliation: {type : String, require:true },
+    coAuthorDetails: {type : Array},
     abstractPaperName: { type: String, require:true },
-    abstractPaperDescription : { type: String, require: true },
+    abstract : { type: String, require: true },
     mimetype: { type: String, require:true },
-    abstractFileUrl : { type: String, require: true },
     userId:{type:String, require:true},
     paperApproveStatus:{type:Boolean, require:true},
     registrationNumber:{type: String, require: true},
+    abstractNumber: {type : String, require: true},
     themeType:{type: String, require: true},
+    paperPresentationType: {type : String, require : true}
 },{timestamps: true} 
 )
 
